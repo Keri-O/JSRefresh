75b494a2df37713f4ec60d45fb90bbe5d574f4a6
Kerikuma <oshilimk@gmail.com>
2026-08-19 00:39:53 -0400

    button event listener

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,10 @@
 const employees = [
-    {id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 }, 
-    {id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 }, 
-    {id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000}
+    {id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization:'Javascript' }, 
+    {id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization:'Python' }, 
+    {id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization:'Java'},
+    {id: 4, name: 'Jane Doe', age: 22, department: 'IT', salary: 50000, specialization:'Javascript' },
+    {id: 5, name: 'James Dow', age: 60, department: 'HR', salary: 100000, specialization:'Python' },
+    {id: 6, name: 'Adam Swam', age: 35, department: 'IT', salary: 50000, specialization:'Javascript' },
 ];
 
 //function to display all employees
@@ -31,4 +34,14 @@ function findEmployeeById(employeeId) {
       else{
         document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
        }
-   }
+}
+
+const specializationButton = document.getElementById("specializationBtn");
+
+specializationButton.addEventListener('click', () => {
+    const jsSpecialization = employees.filter(employee => employee.specialization === 'Javascript');
+    const foundjsEmployees = jsSpecialization.map(employee =>`<p>${employee.id}: ${employee.name} - ${employee.department} - ${employee.specialization}</p>`).join('');
+    
+    document.getElementById('employeesDetails').innerHTML = foundjsEmployees;
+
+})
